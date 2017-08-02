341d95caa62acef605fdcd7ca965d3f006eddacc
Dmitry <dymonius12@gmail.com>
2017-08-02 20:59:27 +0300

    Header and stats remade with FLEX. Fluid was added

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,17 @@
 var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav--closed');
+var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('main-nav--closed')) {
-        navMain.classList.remove('main-nav--closed');
-        navMain.classList.add('main-nav--opened')
-    }
-    else {
-        navMain.classList.remove('main-nav--opened');
-        navMain.classList.add('main-nav--closed')
-    }
-});
+//alert(document.body.clientWidth);
+// if (document.body.clientWidth < 768) {
+    navToggle.addEventListener('click', function () {
+        if (navMain.classList.contains('main-nav--closed')) {
+            navMain.classList.remove('main-nav--closed');
+            navMain.classList.add('main-nav--opened')
+        }
+        else {
+            navMain.classList.remove('main-nav--opened');
+            navMain.classList.add('main-nav--closed')
+        }
+    });
+// }
